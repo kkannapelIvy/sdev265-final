a7bf18af263c8ba752f8cdd891faa864ab23e7e9
Javier Rodriguez <jroddev.droid@gmail.com>
2023-03-28 16:00:18 -0400

    cleared js file

--- script.js
+++
@@ -1,8 +0,0 @@
-const video = document.getElementById("background-video");
-video.addEventListener("click", function() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-});
